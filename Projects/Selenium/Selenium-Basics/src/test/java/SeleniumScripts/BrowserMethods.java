915edcb84c45c335c11c21096eaf323edf526dfe
admin <admin@gmail.com>
2023-09-26 22:01:15 +0530

    Selenium

---
+++ Projects/Selenium/Selenium-Basics/src/test/java/SeleniumScripts/BrowserMethods.java
@@ -0,0 +1,32 @@
+package SeleniumScripts;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class BrowserMethods {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/");
+	     
+	     // Print the title of the webpage
+	     
+	     String title = driver.getTitle();
+	     
+	     System.out.println("The title of the page is : " + title);
+	     
+	     String url = driver.getCurrentUrl();
+	     
+	     System.out.println("The URL of the page is : " + url);
+		
+		// close the browser window
+	     
+	     driver.close();
+	     
+	}
+
+}
