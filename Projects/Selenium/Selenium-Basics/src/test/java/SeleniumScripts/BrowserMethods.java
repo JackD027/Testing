8ad2ae02d95b08c1b6a4fec3e4a896d8327e5cc7
admin <admin@gmail.com>
2023-09-27 13:33:38 +0530

    Selenium updated

--- Projects/Selenium/Selenium-Basics/src/test/java/SeleniumScripts/BrowserMethods.java
+++ Projects/Selenium/Selenium-Basics/src/test/java/SeleniumScripts/BrowserMethods.java
@@ -9,7 +9,7 @@ public class BrowserMethods {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		WebDriverManager.chromedriver().setup();
+	
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.selenium.dev/");
 	     
